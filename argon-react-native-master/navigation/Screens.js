c20df92eb60f83a85e865b518738df383a06c29b
Olivia Gallager <oliviagallager2021@u.northwestern.edu>
2020-02-19 23:42:27 -0600

    hello

---
+++ argon-react-native-master/navigation/Screens.js
@@ -0,0 +1,236 @@
+import React from "react";
+import { Easing, Animated } from "react-native";
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  createAppContainer
+} from "react-navigation";
+
+import { Block } from "galio-framework";
+
+// screens
+import Search from "../screens/Search";
+import Home from "../screens/Home";
+import Pro from "../screens/Pro";
+import Profile from "../screens/Profile";
+import Register from "../screens/Register";
+import Elements from "../screens/Elements";
+import Articles from "../screens/Articles";
+
+import ShoppingCart from "../screens/ShoppingCart";
+<<<<<<< HEAD
+import Itempage from "../screens/Itempage";
+=======
+
+>>>>>>> f4fb1dafa5bc9c75df79b6d2d0362f5b0c09a7c0
+// drawer
+import Menu from "./Menu";
+import DrawerItem from "../components/DrawerItem";
+
+// header for screens
+import Header from "../components/Header";
+
+const transitionConfig = (transitionProps, prevTransitionProps) => ({
+  transitionSpec: {
+    duration: 400,
+    easing: Easing.out(Easing.poly(4)),
+    timing: Animated.timing
+  },
+  screenInterpolator: sceneProps => {
+    const { layout, position, scene } = sceneProps;
+    const thisSceneIndex = scene.index;
+    const width = layout.initWidth;
+
+    const scale = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+      outputRange: [4, 1, 1]
+    });
+    const opacity = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+      outputRange: [0, 1, 1]
+    });
+    const translateX = position.interpolate({
+      inputRange: [thisSceneIndex - 1, thisSceneIndex],
+      outputRange: [width, 0]
+    });
+
+    const scaleWithOpacity = { opacity };
+    const screenName = "Search";
+
+    if (
+      screenName === transitionProps.scene.route.routeName ||
+      (prevTransitionProps &&
+        screenName === prevTransitionProps.scene.route.routeName)
+    ) {
+      return scaleWithOpacity;
+    }
+    return { transform: [{ translateX }] };
+  }
+});
+
+const ElementsStack = createStackNavigator({
+  Elements: {
+    screen: Elements,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Elements" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const ItempageStack = createStackNavigator({
+  Itempage:{
+    screen:Itempage,
+    navigationOptions:({navigation}) => ({
+      header:<Header title="Item Page" navigation={navigation} />
+    })
+  }
+
+});
+
+const ArticlesStack = createStackNavigator({
+  Articles: {
+    screen: Articles,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Articles" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const ShoppingCartStack = createStackNavigator({
+  ShoppingCart: {
+    screen: ShoppingCart,
+    navigationOptions: ({ navigation }) => ({
+      header: <Header title="Shopping Cart" navigation={navigation} />
+    })
+  }
+},{
+  cardStyle: {
+    backgroundColor: "#F8F9FE"
+  },
+  transitionConfig
+});
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: {
+      screen: Profile,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header white transparent title="Profile" iconColor={'#FFF'} navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
+  },
+  {
+    cardStyle: { backgroundColor: "#FFFFFF" },
+    transitionConfig
+  }
+);
+
+const SearchStack = createStackNavigator(
+  {
+    Search: {
+      screen: Search,
+      navigationOptions: ({ navigation }) => ({
+        header: <Header search options title="Search" navigation={navigation} />
+      })
+    },
+    Pro: {
+      screen: Pro,
+      navigationOptions: ({ navigation }) => ({
+        header: (
+          <Header left={<Block />} white transparent title="" navigation={navigation} />
+        ),
+        headerTransparent: true
+      })
+    }
+  },
+  {
+    cardStyle: {
+      backgroundColor: "#F8F9FE"
+    },
+    transitionConfig
+  }
+);
+// divideru se baga ca si cum ar fi un ecrna dar nu-i nimic duh
+const AppStack = createDrawerNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        drawerLabel: () => {}
+      }
+    },
+    Search: {
+      screen: SearchStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Search" title="Home" />
+        )
+      })
+    },
+    Profile: {
+      screen: ProfileStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Profile" title="Profile" />
+        )
+      })
+    },
+    Account: {
+      screen: Register,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Register" title="Account" />
+        )
+      })
+    },
+    Elements: {
+      screen: ElementsStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Elements" title="Elements" />
+        )
+      })
+    },
+    Itempage:{
+      screen:ItempageStack,
+      navigationOptions: navOpt => ({
+         drawerLabel:({focused}) => (
+            <DrawerItem focused={focused} screen="Itempage" title="Itempage" />
+          ) 
+      })
+    },
+    Articles: {
+      screen: ArticlesStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="Articles" title="Articles" />
+        )
+      })
+    },
+    ShoppingCart: {
+      screen: ShoppingCartStack,
+      navigationOptions: navOpt => ({
+        drawerLabel: ({ focused }) => (
+          <DrawerItem focused={focused} screen="ShoppingCart" title="Shopping Cart" />
+        )
+      })
+    }
+  },
+  Menu
+);
+
+const AppContainer = createAppContainer(AppStack);
+export default AppContainer;
