3271374e549b263e038d61489ac2245df09e95c0
Wilson Wong <wilsonwong@Wilsons-MBP.lan>
2020-03-03 08:01:43 -0600

    added more foods

--- argon-react-native-master/data/food_products.js
+++ argon-react-native-master/data/food_products.js
@@ -26,7 +26,7 @@ export default [
     {
         name:"White Buns",
         category:"bread",
-        footprint:15,
+        footprint:12,
         points:-2,
         image: require("../data/images/04.jpg"),
         alternatives: ["Whole Wheat Buns", "Whole Grains Buns"]
@@ -59,7 +59,7 @@ export default [
             fat: "5.5g",
             protien:"31.7g"
         },
-        alternatives: ["Salmon", "Chicken"]
+        alternatives: ["Salmon", "Chicken", "Shrimp"]
     },
     {
         name:"Salmon",
@@ -71,9 +71,9 @@ export default [
             unit: "4 ounces",
             calories: 223.75,
             fat: "9.5g",
-            protien:"32.5g"
+            protein:"32.5g"
         },
-        alternatives: ["Steak", "Chicken"]
+        alternatives: ["Steak", "Chicken", "Shrimp"]
     },
     {
         name:"Chicken",
@@ -81,6 +81,153 @@ export default [
         footprint:18,
         points:4,
         image: require("../data/images/11.jpg"),
-        alternatives: ["Steak", "Salmon"]
-    }
+        nutrition: {
+            unit: "4 ounces",
+            calories: 239,
+            fat: "13.4g",
+            protein:"24g"
+        },
+        alternatives: ["Steak", "Salmon", "Shrimp"]
+    },
+    {
+        name:"Dark Chocolate",
+        category:"Sweets",
+        footprint:19,
+        points:0,
+        image: require("../data/images/23.jpg"),
+        nutrition: {
+            unit: "1 ounce",
+            calories: 170,
+            fat: "13g",
+            protein:"2g"
+        },
+        alternatives: ["Milk Chocolate"]
+    },
+
+    {
+        name:"Milk Chocolate",
+        category:"Sweets",
+        footprint:17,
+        points:2,
+        image: require("../data/images/12.jpg"),
+        nutrition: {
+            unit: "1 ounce",
+            calories: 128,
+            fat: "8g",
+            protein:"1.3g"
+        },
+        alternatives: ["Dark Chocolate"]
+    },
+    {
+        name:"Eggs",
+        category:"Dairy",
+        footprint:15,
+        points:4,
+        image: require("../data/images/14.jpg"),
+        nutrition: {
+            unit: "1 egg, boiled",
+            calories: 78,
+            fat: "5g",
+            protein:"6g"
+        },
+        alternatives: ["Cheese", "Tofu"]
+    },
+    {
+        name:"Cheese",
+        category:"Dairy",
+        footprint:16,
+        points:3,
+        image: require("../data/images/16.jpg"),
+        nutrition: {
+            unit: "1 slice",
+            calories: 98,
+            fat: "7g",
+            protein:"7g"
+        },
+        alternatives: ["Eggs", "Tofu"]
+    },
+    {
+        name:"Tofu",
+        category:"Dairy",
+        footprint:9,
+        points:10,
+        image: require("../data/images/18.jpg"),
+        nutrition: {
+            unit: "0.5cup",
+            calories: 94,
+            fat: "6g",
+            protein:"10g"
+        },
+        alternatives: ["Eggs", "Cheese"]
+    },
+    {
+        name:"Shrimp",
+        category:"meat",
+        footprint:25,
+        points:-5,
+        image: require("../data/images/15.jpg"),
+        nutrition: {
+            unit: "100g",
+            calories: 99,
+            fat: "0.3g",
+            protein:"24g"
+        },
+        alternatives: ["Steak", "Salmon", "Chicken"]
+    },
+    {
+        name:"Coffee",
+        category:"Beverage",
+        footprint:11,
+        points:5,
+        image: require("../data/images/13.jpg"),
+        nutrition: {
+            unit: "1 cup",
+            calories: 1,
+            fat: "0g",
+            protein:"0g"
+        },
+        alternatives: ["Beer"]
+    },
+    {
+        name:"Beer",
+        category:"Beverage",
+        footprint:16,
+        points:-5,
+        image: require("../data/images/17.jpg"),
+        nutrition: {
+            unit: "1 can",
+            calories: 154,
+            fat: "0g",
+            protein:"1.6g"
+        },
+        alternatives: ["Coffee"]
+    },
+    {
+        name:"Potatoes",
+        category:"Carbs",
+        footprint:2,
+        points:12,
+        image: require("../data/images/20.jpg"),
+        nutrition: {
+            unit: "1 Medium Potato",
+            calories: 163,
+            fat: "0.2g",
+            protein:"4.3g"
+        },
+        alternatives: ["Rice"]
+    },
+    {
+        name:"Rice",
+        category:"Carbs",
+        footprint:10,
+        points:5,
+        image: require("../data/images/21.jpg"),
+        nutrition: {
+            unit: "1 cup cooked",
+            calories: 206,
+            fat: "0.4g",
+            protein:"4.3g"
+        },
+        alternatives: ["Potatoes"]
+    },
   ];
--- argon-react-native-master/data/trivia.js
+++ argon-react-native-master/data/trivia.js
@@ -5,7 +5,7 @@ export default [
         footprint1: 100,
         name2:"Ground Turkey" ,
         image2: require("../data/images/02.jpg"),
-        footprint2: 50   
+        footprint2: 50
     },
     {
         name1: "White Buns" ,
@@ -13,22 +13,63 @@ export default [
         footprint1: 15,
         name2:"Whole Wheat Buns" ,
         image2: require("../data/images/05.jpg"),
-        footprint2: 10   
+        footprint2: 10
     },
     {
         name1: "Salmon" ,
         image1: require("../data/images/10.jpg"),
         footprint1: 20,
         name2:"Chicken" ,
-        image2: require("../data/images/15.jpg"),
-        footprint2: 18   
-    }, 
+        image2: require("../data/images/11.jpg"),
+        footprint2: 18
+    },
     {
         name1: "Soy Milk" ,
         image1: require("../data/images/04.jpg"),
         footprint1: 10,
         name2:"Almond Milk" ,
         image2: require("../data/images/05.jpg"),
-        footprint2: 8   
-    }
+        footprint2: 8
+    },
+    {
+        name1: "Dark Chocolate" ,
+        image1: require("../data/images/23.jpg"),
+        footprint1: 19,
+        name2:"Milk Chocolate" ,
+        image2: require("../data/images/12.jpg"),
+        footprint2: 17
+    },
+    {
+        name1: "Salmon" ,
+        image1: require("../data/images/10.jpg"),
+        footprint1: 20,
+        name2:"Shrimp" ,
+        image2: require("../data/images/15.jpg"),
+        footprint2: 25
+    },
+    {
+        name1: "Eggs" ,
+        image1: require("../data/images/14.jpg"),
+        footprint1: 15,
+        name2:"Cheese" ,
+        image2: require("../data/images/16.jpg"),
+        footprint2: 16
+    },
+    {
+        name1: "Coffee" ,
+        image1: require("../data/images/13.jpg"),
+        footprint1: 11,
+        name2:"Beer" ,
+        image2: require("../data/images/17.jpg"),
+        footprint2: 16
+    },
+    {
+        name1: "Potatoes" ,
+        image1: require("../data/images/20.jpg"),
+        footprint1: 2,
+        name2:"Rice" ,
+        image2: require("../data/images/21.jpg"),
+        footprint2: 10
+    },
+
 ];
